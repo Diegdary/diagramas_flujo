148d1d090af0ca0007c358f66ac0404503f345dc
Diegdary <diegojose3019@gmail.com>
2024-10-31 00:42:22 -0500

    agregacion de valores estilo javascript (cambiar a estilo react)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './InOut.css'
 
 function App() {
-  
-  const altermatrix = ()=>{
 
+  
+  
+  const addsingle = ()=>{
+    document.getElementById("matrix")!.innerHTML += `
+    <div class='inputcontainer'>
+              <input type="text" class='valores'/>
+          </div>
+          <div class='inputcontainer'>
+              <input type="text" class='valores'/>
+          </div>
+          <div class='inputcontainer'>
+              <input type="text" class='valores'/>
+          </div>
+    ` ;
   }
 
   return (
@@ -16,23 +28,26 @@ function App() {
       <form action="" className='wholeinput'>
         <div className='element_periodo'>
           <label htmlFor="n_periodos">Periodos:</label>
-          <input type="number" name="periodos" id="n_periodos" onChange={altermatrix} />
+          <input type="number" name="periodos" id="n_periodos" />
         </div>
-        <div className='matrix'>
-          <div></div>
-          <div>up</div>
-          <div>down</div>
-          <div>1</div>
-          <div>x</div>
-          <div>0.2x</div>
-          <div>2</div>
-          <div>1000</div>
-          <div></div>
-          <div>3</div>
-          <div></div>
-          <div>400</div>
+        <div className='matrix' id='matrix'>
+          <div>valor</div>
+          <div>Ingreso</div>
+          <div>Egreso</div>
+          <div className='inputcontainer'>
+              <input type="text" className='valores'/>
+          </div>
+          <div className='inputcontainer'>
+              <input type="text" className='valores'/>
+          </div>
+          <div className='inputcontainer'>
+              <input type="text" className='valores'/>
+          </div>
         </div>
-
+        <div className='addcontainer'>
+          <button type="button" id='addbutton' onClick={addsingle}>Add</button>
+        </div>
+        
       </form>
     </>
   )
