81ea67f7529fac9e13d868cfff345ce956e8dbe9
Diegdary <diegojose3019@gmail.com>
2024-11-01 23:02:50 -0500

    botones de agregar y eliminar + cambio de periodo desde input funcionando (odio react)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, ChangeEvent } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,21 +6,51 @@ import './InOut.css'
 
 function App() {
 
+  const [elementos, setelementos] = useState(["","",""]);
   
-  
-  const addsingle = ()=>{
-    document.getElementById("matrix")!.innerHTML += `
-    <div class='inputcontainer'>
-              <input type="text" class='valores'/>
-          </div>
-          <div class='inputcontainer'>
-              <input type="text" class='valores'/>
-          </div>
-          <div class='inputcontainer'>
-              <input type="text" class='valores'/>
-          </div>
-    ` ;
-  }
+  const addsingle = (n: number)=>{
+    setelementos(last=>{
+      let newer = [...last];
+      for (let i = 0; i < n; i++) {
+        newer.splice(newer.length,0,"","","");
+      }
+      return newer;
+    });
+  };
+
+  const removesingle = (n: number)=>{
+
+    setelementos(last=>{
+      let newer = [...last];
+      if(newer.length!=3){
+        for (let i = 0; i < n; i++) {
+          newer.splice(newer.length-3,3);
+        }
+      }
+      return newer;
+    });
+  };
+
+
+  const instantinput = (event: ChangeEvent<HTMLInputElement>)=>{
+    let inp :string  = event.target.value;
+    let numb = inp == ""? 1 : parseInt(inp);
+    if (numb >0) {
+      document.getElementById("validation")!.innerHTML="";
+      let lastsize:number =elementos.length/3;
+      if (numb>lastsize) {
+        console.log(lastsize + "<" + numb);
+        addsingle(numb-lastsize);
+      }
+      else{
+        console.log(lastsize + ">" + numb);
+        removesingle(lastsize-numb);
+      }
+    }
+    else{
+      document.getElementById("validation")!.innerHTML="No válido";
+    }
+  };
 
   return (
     <>
@@ -28,24 +58,20 @@ function App() {
       <form action="" className='wholeinput'>
         <div className='element_periodo'>
           <label htmlFor="n_periodos">Periodos:</label>
-          <input type="number" name="periodos" id="n_periodos" />
+          <input type="number" name="periodos" id="n_periodos" min={1} defaultValue={1} onChange={instantinput}/>
+          <p id='validation'></p>
         </div>
         <div className='matrix' id='matrix'>
-          <div>valor</div>
-          <div>Ingreso</div>
-          <div>Egreso</div>
-          <div className='inputcontainer'>
-              <input type="text" className='valores'/>
-          </div>
-          <div className='inputcontainer'>
-              <input type="text" className='valores'/>
-          </div>
-          <div className='inputcontainer'>
-              <input type="text" className='valores'/>
-          </div>
+          <div className='valores layers'> Valores </div>
+          <div className='valores layers'> Ingreso </div>
+          <div className='valores layers'> Egreso </div>
+          {elementos.map((item,index)=>
+            <div key={index}><input type="text" className='valores' defaultValue={item} /></div>
+          )}
         </div>
         <div className='addcontainer'>
-          <button type="button" id='addbutton' onClick={addsingle}>Add</button>
+          <button type="button" id='addbutton' onClick={()=>{addsingle(1)}}>Add</button>
+          <button type="button" id='deltbutton' onClick={()=>{removesingle(1)}}>Delete</button>
         </div>
         
       </form>
