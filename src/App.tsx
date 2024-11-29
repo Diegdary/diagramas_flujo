816356cd6904b9b5cca5bfc2002f3b27780c7559
David <davida-osorioqunilibre.edu.co>
2024-11-29 07:37:32 -0500

    lll

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState, useRef, ChangeEvent, createElement } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import "./InOut.css";
 
